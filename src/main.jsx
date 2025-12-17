45f7511e15c843a27aef29fb1d07586e89fc850a
agudu50 <anthonygudu2020@gmail.com>
2025-12-17 15:34:39 +0000

    fixed deployment bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { Router } from './components/Router.jsx'; // import your custom Router
+// App already includes the Router provider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </StrictMode>
 );
